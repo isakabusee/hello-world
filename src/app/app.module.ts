6e2727f67d414b1a49b016579f8fc38db1b589f9
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2021-01-29 14:37:40 -0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
+import { CoursesService } from './courses.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses.component';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseComponent,
+    CoursesComponent
+    //second step is to register your component by adding it here. Third step go to app.component.HTML 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    CoursesService
+    // this is dependancy injection from course component in the constructor section
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
